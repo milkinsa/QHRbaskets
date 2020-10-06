f8d075389667d2af4f4740c3a311b504d7ba7dc7
milkins <milen.milev.96@gmail.com>
2020-10-06 18:15:20 +0300

    config localhost remoced

--- config.go
+++ config.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	defaultServicePort  = 55555
-	defaultServiceAddr  = "127.0.0.1"
+	defaultServiceAddr  = ":"
 	defaultPageSize     = 20
 	initBasketCapacity  = 200
 	maxBasketCapacity   = 2000
